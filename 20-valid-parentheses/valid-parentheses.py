ad146b1cd9486be4c1c5cfb2effafbf694c63afb
Ashish Meshram <iashishmeshram@gmail.com>
2026-05-17 07:14:24 +0530

    Time: 3 ms (34.59%) | Memory: 19.3 MB (66.99%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s):
+        stack = []
+        mp = {')':'(', '}':'{', ']':'['}
+
+        for ch in s:
+            if ch in mp.values():
+                stack.append(ch)
+            else:
+                if not stack or stack.pop() != mp[ch]:
+                    return False
+
+        return len(stack) == 0
