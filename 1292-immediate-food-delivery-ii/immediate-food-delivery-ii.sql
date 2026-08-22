f57534623610d735ff69ba15d0965037d25971d0
Ashish Meshram <iashishmeshram@gmail.com>
2026-08-22 13:51:05 +0530

    Time: 680 ms (58.62%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -0,0 +1,18 @@
+WITH first_orders AS (
+    SELECT
+        customer_id,
+        MIN(order_date) AS first_order
+    FROM Delivery
+    GROUP BY customer_id
+)
+SELECT
+    ROUND(
+        100 * AVG(
+            d.order_date = d.customer_pref_delivery_date
+        ),
+        2
+    ) AS immediate_percentage
+FROM Delivery d
+JOIN first_orders f
+    ON d.customer_id = f.customer_id
+   AND d.order_date = f.first_order;
